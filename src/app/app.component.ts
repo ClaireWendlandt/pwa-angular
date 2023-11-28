46f95a4bcde3ec84b17549ac9b62c084fe7d3dd8
Claire <cwendlandt@webnet.fr>
2023-11-28 20:01:38 +0100

    service worker ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CheckForUpdateService } from './services/check-for-update.service';
+import { LogUpdateService } from './services/log-update.service';
+import { PromptUpdateService } from './services/prompt-update.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'pwa-angular';
+  title = 'pwa-angudldkllar';
+
+  constructor(
+    private logUpdateService: LogUpdateService,
+    private checkForUpdate: CheckForUpdateService,
+    private promptUpdateService: PromptUpdateService
+  ) {}
 }
---
+++ src/app/services/check-for-update.service.ts
@@ -0,0 +1,35 @@
+import { ApplicationRef, Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { concat, interval } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class CheckForUpdateService {
+  constructor(appRef: ApplicationRef, updates: SwUpdate) {
+    console.log('in check for update');
+    // Allow the app to stabilize first, before starting
+    // polling for updates with `interval()`.
+    const appIsStable$ = appRef.isStable.pipe(
+      first((isStable) => isStable === true)
+    );
+    const everySixHours$ = interval(1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+    everySixHoursOnceAppIsStable$.subscribe(async () => {
+      try {
+        const updateFound = await updates.checkForUpdate();
+
+        console.log(
+          updateFound
+            ? 'A new version is available.'
+            : 'Already on the latest version.'
+        );
+
+        if (updateFound) {
+          document.location.reload();
+        }
+      } catch (err) {
+        console.error('Failed to check for updates:', err);
+      }
+    });
+  }
+}
